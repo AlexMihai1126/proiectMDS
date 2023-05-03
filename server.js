2391974df82d92e9cbf544bb142f8cc79fdfb9c0
AlexMihai1126 <115073416+AlexMihai1126@users.noreply.github.com>
2023-05-03 10:23:51 +0300

    signup si login functionale

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const { Client } = require("pg");
 const bcr = require("bcrypt");
 const session = require("express-session");
 const flash = require("express-flash");
+const passport = require("passport");
+const initPassport = require("./passportConfig");
+
+initPassport(passport);
 
 var client= new Client({database:"mds",
         user:"alexm1126",
@@ -23,6 +27,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 app.get(["/","/home","/index"], function(req, res){
@@ -42,21 +48,11 @@ app.get("/utilizator/home", function(req, res){
 });
 
 app.post("/utilizator/signup", async function(req, res){
-    let {nume, prenume, email, parola, parola_conf, tel, addr} = req.body;
-        /* console.log({
-            nume,
-            prenume,
-            email,
-            parola,
-            parola_conf,
-            tel,
-            addr
-        }); */
-
-    let err = [];
+    let {nume, prenume, email, parola, parola_conf, tel, addr} = req.body; //citim din formular in acest obiect
+    let err = []; //vectorul de erori pe care il vom trimite catre ejs pt afisare
     if(!nume || !prenume || !email || !parola || !parola_conf){
         err.push({mesaj:"Va rugam introduceti toate datele necesare!"});
-    };
+    }; //daca nu exista da eroare (am rezolvat cu validare la nivel de formular)
 
     if(parola.length <8){
         err.push({mesaj:"Parola trebuie sa aiba minim 8 caractere!"});
@@ -67,7 +63,7 @@ app.post("/utilizator/signup", async function(req, res){
     }
 
     if(err.length>0){
-        res.render("pages/signup", {err})
+        res.render("pages/signup", {err}); //daca avem erori vom regenera pagina cu erorile afisate
     } else{
         let hashPass= await bcr.hash(parola, 10); //cripteaza parola cu 10 runde de criptare in algoritm
         client.query(
@@ -84,9 +80,9 @@ app.post("/utilizator/signup", async function(req, res){
                         `INSERT INTO utilizatori (nume, prenume, email, parola, telefon, adresa) values($1, $2, $3, $4, $5, $6) RETURNING id, parola`, [nume, prenume, email, hashPass, tel, addr], function(queryErrIns, queryResIns){
                             if(queryErrIns){
                                 throw queryErrIns;
-                            }
+                            } //cererea in baza de date care introduce noul user
                             req.flash("succes", "Contul a fost creat");
-                            res.redirect("/utilizator/login");
+                            res.redirect("/utilizator/login"); //redirectioneaza catre pagina de login
                         }
                     )
                 }
@@ -95,7 +91,15 @@ app.post("/utilizator/signup", async function(req, res){
     }
 
     // console.log(err);
-})
+});
+
+app.post("/utilizator/login", passport.authenticate('local', {
+    successRedirect: '/utilizator/home',
+    failureRedirect: '/utilizator/login',
+    failureFlash: true
+})); //folosim libraria passport pentru a autentifica utilizatorul si crea cookie-ul
+
+// /utilizator/logout
 
 app.listen(PORT, ()=>{
     console.log(`Serverul a pornit, port: ${PORT}`);
